2dad384a39cc01e26ef38ed519922a17611c2228
Marian Simo <sim0302@vsb.cz>
2022-11-14 22:40:49 +0100

    Added views

--- RunGroupWebApp/Controllers/ClubController.cs
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
 
 namespace RunGroupWebApp.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ApplicationDbContext _context; //_db
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Detail(int id) 
+        {
+            var club = _context.Clubs.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+            return View(club);
         }
     }
 }
--- RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Data;
 
 namespace RunGroupWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var race = _context.Races.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+            return View(race);
         }
     }
 }
