a9137a62093f2bd58ce55d2c46f504f796c73f71
Marian Simo <sim0302@vsb.cz>
2022-11-23 23:11:40 +0100

    Added user repo controller and view

---
+++ RunGroupWebApp/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data.Interfaces;
+using RunGroupWebApp.ViewModels;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet("users")]
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsers();
+            List<UserViewModel> result = new List<UserViewModel>();
+            foreach(var user in users)
+            {
+                var userVM = new UserViewModel()
+                {
+                    Id = user.Id,
+                    UserName = user.UserName,
+                    Pace = user.Pace,
+                    Mileage = user.Mileage
+                };
+                result.Add(userVM);
+            }
+            return View(result);
+        }
+
+
+        public async Task<IActionResult> Detail(string id)
+        {
+            var user = await _userRepository.GetUserById(id);
+            var userDetailViewModel = new UserDetailViewModel()
+            {
+                Id = user.Id,
+                Pace = user.Pace,
+                Mileage = user.Mileage,
+                UserName = user.UserName
+            };
+            return View(userDetailViewModel);
+        }
+
+    }
+}
